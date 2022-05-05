99822a478e969586dd52c43b854d4b6c7134aac1
pedromtsousa18 <104302299+pedromtsousa18@users.noreply.github.com>
2022-05-05 23:02:32 +0100

    Add files via upload

---
+++ View_Brick.sql
@@ -0,0 +1,14 @@
+
+
+CREATE OR ALTER VIEW.BRICK AS
+(
+SELECT SC.CustomerID, StoreID, BusinessEntityID, PB.AddressID, PA.City, SOH.SubTotal
+FROM Sales.Customer AS SC
+LEFT JOIN Person.BusinessEntityAddress AS PB
+ON SC.StoreID = PB.BusinessEntityID
+LEFT JOIN Person.Address AS PA
+ON PA.AddressID = PB.AddressID
+LEFT JOIN Sales.SalesOrderHeader AS SOH
+ON SC.CustomerID = SOH.CustomerID
+WHERE SC.StoreID IS NOT NULL
+)
